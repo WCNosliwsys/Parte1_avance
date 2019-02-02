5e5d9112257233577cb24fb8faac9e2f3e57981f
Ing Wilson <nosliwsys@gmail.com>
2019-02-02 13:03:08 -0500

    Ciclo de vida

---
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/parte1_avance/AcercaDe.java
@@ -0,0 +1,14 @@
+package com.a952000243.ingwilson.nosliwsys.parte1_avance;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcercaDe  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
