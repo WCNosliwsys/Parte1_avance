c9194308067aaa9fc89941e310ed741fb767e45a
Ing Wilson <nosliwsys@gmail.com>
2019-02-02 11:50:53 -0500

    FragmentTabhost parte1

--- app/src/main/java/com/a952000243/ingwilson/nosliwsys/parte1_avance/Ejemplo3Activity.java
+++ app/src/main/java/com/a952000243/ingwilson/nosliwsys/parte1_avance/Ejemplo3Activity.java
@@ -3,11 +3,11 @@ package com.a952000243.ingwilson.nosliwsys.parte1_avance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Ejemplo3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ejemplo2);
+        setContentView(R.layout.activity_ejemplo3);
     }
 }
